9c79566bca97592ead30b034325a75070c518257
kimjunha <rlawnsgk0202@hs.ac.kr>
2025-10-20 19:33:07 +0900

    초기 화면 매니저 선택

--- App.js
+++ App.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
 import OnboardingScreen from './components/OnboardingScreen';
 import LoginScreen from './screens/Login/LoginScreen';
-import SignupScreen1 from './screens/Register/SignupScreen';
+import SignupScreen from './screens/Register/SignupScreen.js';
 import SignupScreen2 from './screens/Register/SignupScreen2';
 import SignupScreen3 from './screens/Register/SignupScreen3';
 import SignupScreen4 from './screens/Register/SignupScreen4';
 import SignupScreen5 from './screens/Register/SignupComplete';
+import RoleSelection from './screens/Register/RoleSelection.js';
+import ManagerSignupScreen from './screens/Register/ManagerSignupScreen.js';
 import MainScreen from './screens/MainScreen';
 import ClubScreen from './screens/Club/ClubScreen';
 import MyClubScreen from './screens/Club/MyClubScreen';
@@ -36,6 +37,7 @@ import WalkRecordScreen from './screens/Feature/WalkRecordScreen';
 import MedicineScreen from './screens/Feature/MedicineScreen';
 import MedicineNewScreen from './screens/Feature/MedicineNewScreen';
 
+
 const Stack = createNativeStackNavigator();
 
 export default function App() {
@@ -44,11 +46,13 @@ export default function App() {
       <Stack.Navigator initialRouteName="Onboarding" screenOptions={{ headerShown: false }}>
         <Stack.Screen name="Onboarding" component={OnboardingScreen} />
         <Stack.Screen name="Login" component={LoginScreen} />
-        <Stack.Screen name="Signup1" component={SignupScreen1} />
+        <Stack.Screen name="RoleSelection" component={RoleSelection} />
+        <Stack.Screen name="Signup1" component={SignupScreen} />
         <Stack.Screen name="Signup2" component={SignupScreen2} />
         <Stack.Screen name="Signup3" component={SignupScreen3} />
         <Stack.Screen name="Signup4" component={SignupScreen4} />
         <Stack.Screen name="Signup5" component={SignupScreen5} />
+        <Stack.Screen name="ManagerSignupScreen" component={ManagerSignupScreen} />
         <Stack.Screen name="Main" component={MainScreen} />
         <Stack.Screen name="Club" component={ClubScreen} />
         <Stack.Screen name="MyClub" component={MyClubScreen} />
